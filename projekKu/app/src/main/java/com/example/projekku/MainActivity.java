b14336a7b71d1efb02fd51e002f88c1110af8a2b
UsmanAlwanSya <oesman198@gmail.com>
2021-10-14 21:04:09 +0700

    file tugas

---
+++ projekKu/app/src/main/java/com/example/projekku/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.projekku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText angk1,angk2;
+    TextView hasilnya;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        angk1=(EditText) findViewById(R.id.angka1);
+        angk2=(EditText) findViewById(R.id.angka2);
+        hasilnya=(EditText) findViewById(R.id.hasilhitung);
+    }
+
+    public void hitung(View view) {
+    int a1=Integer.parseInt(angk1.getText().toString());
+    int a2=Integer.parseInt(angk2.getText().toString());
+    integer hasil=a1+a2;
+    hasilnya.setText(hasil.toString());
+    }
+}
